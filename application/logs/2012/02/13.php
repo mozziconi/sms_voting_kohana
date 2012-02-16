ca44afc1d384cac730bca826efe7471be5d848d7
Grigory Pashukhin <pashukhin@gmail.com>
2012-02-16 21:28:49 +0400

    .gitignore - cache and logs

--- application/logs/2012/02/13.php
+++
@@ -1,7 +0,0 @@
-<?php defined('SYSPATH') or die('No direct script access.'); ?>
-
-2012-02-13 14:40:54 --- ERROR: HTTP_Exception_404 [ 404 ]: Unable to find a route to match the URI: favicon.ico ~ SYSPATH/classes/kohana/request.php [ 1126 ]
-2012-02-13 14:40:54 --- STRACE: HTTP_Exception_404 [ 404 ]: Unable to find a route to match the URI: favicon.ico ~ SYSPATH/classes/kohana/request.php [ 1126 ]
---
-#0 /var/www/sms_voting_kohana/index.php(109): Kohana_Request->execute()
-#1 {main}
