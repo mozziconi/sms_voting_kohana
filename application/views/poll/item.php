b1804852451f07d080c717327667c9f1912c265c
Grigory Pashukhin <pashukhin@gmail.com>
2012-02-22 17:25:26 +0400

    static tabs

--- application/views/poll/item.php
+++ application/views/poll/item.php
@@ -2,12 +2,30 @@
 <p class="date">Начало - <?=date('d.m.Y H:i:s', strtotime($poll->start_date))?></p>
 <?=$poll->end_date?"<p class=\"date\">Окончание - ".date('d.m.Y H:i:s', strtotime($poll->end_date))."</p>":''?> -->
 <div id="poll_<?=$poll->id?>_tabs" class="poll_tabs">
-	<ul>
+	<!-- ul>
 		<li><a href="/poll/<?=$poll->id?>/description">Описание</a></li>
 		<li><a href="/poll/<?=$poll->id?>/answers">Голосование</a></li>
 		<li><a href="/poll/<?=$poll->id?>/regions">Регионы</a></li>
 		<li><a href="/poll/<?=$poll->id?>/log">Лог голосования</a></li>
+	</ul -->
+	<ul>
+		<li><a href="#tabs_<?=$poll->id?>_description">Описание</a></li>
+		<li><a href="#tabs_<?=$poll->id?>_answers">Голосование</a></li>
+		<li><a href="#tabs_<?=$poll->id?>_regions">Регионы</a></li>
+		<li><a href="#tabs_<?=$poll->id?>_log">Лог голосования</a></li>
 	</ul>
+  <div class="poll_tab" id="#tabs_<?=$poll->id?>_description">
+    <?php echo View::factory('poll/description')->bind('poll',$poll); ?>
+  </div>
+  <div class="poll_tab" id="#tabs_<?=$poll->id?>_answers">
+    <?php echo View::factory('poll/answers')->bind('poll',$poll); ?>
+  </div>
+  <div class="poll_tab" id="#tabs_<?=$poll->id?>_regions">
+    <?php echo View::factory('poll/regions')->bind('poll',$poll); ?>
+  </div>
+  <div class="poll_tab" id="#tabs_<?=$poll->id?>_log">
+    <?php echo View::factory('poll/log')->bind('poll',$poll); ?>
+  </div>
 </div>
 <script>
 	$( "#poll_<?=$poll->id?>_tabs" ).tabs();
