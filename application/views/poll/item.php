df9821ff2aba289c8f96f4667e816a6eb5c97696
Grigory Pashukhin <pashukhin@gmail.com>
2012-02-22 17:33:33 +0400

    single region - 1

--- application/views/poll/item.php
+++ application/views/poll/item.php
@@ -11,16 +11,16 @@
 		<li><a href="#poll_<?=$poll->id?>_tabs_regions">Регионы</a></li>
 		<li><a href="#poll_<?=$poll->id?>_tabs_log">Лог голосования</a></li>
 	</ul>
-  <div class="poll_tab" id="#poll_<?=$poll->id?>_tabs_description">
+  <div class="poll_tab" id="poll_<?=$poll->id?>_tabs_description">
     <?php echo View::factory('poll/description')->bind('poll',$poll); ?>
   </div>
-  <div class="poll_tab" id="#poll_<?=$poll->id?>_tabs_answers">
+  <div class="poll_tab" id="poll_<?=$poll->id?>_tabs_answers">
     <?php echo View::factory('poll/answers')->bind('poll',$poll); ?>
   </div>
-  <div class="poll_tab" id="#poll_<?=$poll->id?>_tabs_regions">
+  <div class="poll_tab" id="poll_<?=$poll->id?>_tabs_regions">
     <?php echo View::factory('poll/regions')->bind('poll',$poll); ?>
   </div>
-  <div class="poll_tab" id="#poll_<?=$poll->id?>_tabs_log">
+  <div class="poll_tab" id="poll_<?=$poll->id?>_tabs_log">
     <?php echo View::factory('poll/log')->bind('poll',$poll); ?>
   </div>
 </div>
