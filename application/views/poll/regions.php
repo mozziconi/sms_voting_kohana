8ca37f1690f7f27d65b8954bc2af73e43dd53bb0
Grigory Pashukhin <pashukhin@gmail.com>
2012-02-22 17:27:16 +0400

    single region - 1

--- application/views/poll/regions.php
+++ application/views/poll/regions.php
@@ -1,6 +1,7 @@
 <h3>Это голосование проводится для следующих регионов</h3>
 <ul>
 <?php
+echo $poll->regions->count_all();
 foreach ($poll->regions->order_by('name','asc')->find_all() as $region)
 {
 	?>
