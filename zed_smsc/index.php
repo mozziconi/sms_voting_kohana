5b8d10b3067a05bb87c51ddc99d71b701d845012
Grigory Pashukhin <pashukhin@gmail.com>
2012-02-22 13:05:31 +0400

    zed sms sending

--- zed_smsc/index.php
+++ zed_smsc/index.php
@@ -19,6 +19,8 @@ else
   $protocol = 'test-protocol';
 };
 
+echo "\n<response-poll uid=\”$uid\” protocol=”\$protocol\” next-request-period=\”60\”>\n";
+
 // connect to mysql
 $connection = mysql_connect('localhost', 'root', 'password') or ($message = mysql_error());
 mysql_set_charset('utf8') or ($message = mysql_error());
@@ -26,30 +28,19 @@ mysql_select_db('sms_voting') or ($message = mysql_error());
 
 $q = 'select * from `messages` order by `created` asc limit 30';
 $messages = mysql_query($q);
-?>
-<response-poll uid=”<?=$uid?>” protocol=”<?=$protocol?>” next-request-period=”60”>
-<?php
 $ids = array();
 if(mysql_num_rows($messages))
 {
   while($message = mysql_fetch_object($messages))
   {
-    ?>
-  <message-poll>
-    <abonent isnn=”ИСНН” phone=”<?=$message->phone?>” operator=”оператор”/>
-    <content-text content-type="text/plain"><?=$message->message?></content-text>
-  </message-poll>
-    <?php
+    echo "\t<message-poll>\n\t\t<abonent isnn=\”ИСНН” phone=\”{$message->phone}”/>\n\t\t<content-text content-type=\"text/plain\">{$message->message}</content-text>\n\t</message-poll>\n";
     $ids[] = $message->id;
   }
-  $q = 'delete from messages where id in ('.implode(',',$ids).')';
+  $q = 'delete from `messages` where `id` in ('.implode(',',$ids).')';
   mysql_query($q);
 }
 else
 {
-  ?>
-  <noreply/>
-  <?php
+  echo "\t<noreply/>\n"
 }
-?>
-</response-poll>
+echo "</response-poll>";
